23e696b7cb9f7733c67d1caa16faaf87e6c85bdc
Harsha <shreeharsha.a@improve10x.com>
2023-03-14 14:31:05 +0530

    pushed to github

---
+++ app/src/main/java/com/example/fooddelivery/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.example.fooddelivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+    }
+}
